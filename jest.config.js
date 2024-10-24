15e2058c55d0d76ab08659c88d7451d764578dbc
Brian Robles <brobles39@hotmail.com>
2024-10-23 23:32:52 -0500

    fixing details

--- jest.config.js
+++ jest.config.js
@@ -2,4 +2,15 @@ module.exports = {
   testEnvironment: 'node',
   testMatch: ['**/*.test.js'],
   moduleFileExtensions: ['js', 'json'],
+  collectCoverage: true,  // Enables coverage collection
+  coverageDirectory: 'coverage',  // Directory where coverage reports will be saved
+  coverageThreshold: {
+    global: {
+      branches: 80,  // Minimum threshold for branch coverage
+      functions: 80, // Minimum threshold for function coverage
+      lines: 80,     // Minimum threshold for line coverage
+      statements: 80 // Minimum threshold for statement coverage
+    }
+  },
+  coverageReporters: ['json', 'lcov', 'text', 'clover'],  // Types of coverage reports to generate
 };
